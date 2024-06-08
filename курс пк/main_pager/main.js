9dc14d4f3256e98fd2646df0d2b787ba3a7a4b1a
VELKOST <vlad.podyachev001@mail.ru>
2024-06-08 16:02:30 +0300

    Front final

--- курс пк/main_pager/main.js
+++
@@ -1,8 +0,0 @@
-// main.js
-document.addEventListener('DOMContentLoaded', function () {
-    const myButton = document.getElementById('myButton');
-    myButton.addEventListener('click', function () {
-        const sectionCallus = document.querySelector('.galleryimgs');
-        sectionCallus.scrollIntoView({ behavior: 'smooth' });
-    });
-});
